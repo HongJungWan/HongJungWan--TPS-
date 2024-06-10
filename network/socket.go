ee51ec919050daec738a0a302e8f97350e9b8198
jungwanhong-metanet <jungwan.hong.metanet@joyent.com>
2024-06-11 08:52:06 +0900

    feat: 채팅방 구성을 위한 기본 객체 설정하기

---
+++ network/socket.go
@@ -0,0 +1,26 @@
+package network
+
+import "golang.org/x/net/websocket"
+
+type Room struct {
+	Forward chan *Message // 수신되는 메시지를 보관하는 값
+	// 들어오는 메시지를 다른 클라이언트에게 전송을 한다.
+
+	Join  chan *Client // Socket이 연결되는 경우에 동작
+	Leave chan *Client // Socket이 끊어지는 경우에 동작
+
+	Clients map[*Client]bool // 현재 방에 있는 Client 정보를 저장
+}
+
+type Message struct {
+	Name            string
+	ChattingMessage string
+	Time            int64
+}
+
+type Client struct {
+	Send   chan *Message
+	Room   *Room
+	Name   string
+	Socket *websocket.Conn
+}
