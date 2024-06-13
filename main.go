fbe9bc0343c484987e49787bd7358676472a9413
HongJungWan <76596316+HongJungWan@users.noreply.github.com>
2024-06-13 21:51:09 +0900

    feat: flag 셋팅 (#10)

--- main.go
+++ main.go
@@ -1,15 +1,16 @@
 package main
 
 import (
-	"go_chat/network"
-	"log"
+	"flag"
+	"fmt"
+	config "go_chat/config"
 )
 
-func init() {
-	log.Println("init test")
-}
+var pathFlag = flag.String("config", "./config.toml", "config set")
+var port = flag.String("port", ":1010", "port set")
 
 func main() {
-	network := network.NewServer()
-	network.StartServer()
+	flag.Parse()
+	configSetUp := config.NewConfig(*pathFlag)
+	fmt.Println(configSetUp)
 }
