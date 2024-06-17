ec8aeca4e9fea07bde8aca7b1d16d9158ac5c678
HONG-JUNG-WAN <hong43ok@gmail.com>
2024-06-17 19:56:20 +0900

    feat: 3 tier architecture

--- network/server.go
+++ network/server.go
@@ -1,8 +1,32 @@
 package network
 
+import "github.com/gin-gonic/gin"
+
 type api struct {
 	server *Server
 }
 
 func registerServer(server *Server) {
+	a := &api{server: server}
+
+	server.engine.GET("/room-list", a.roomList)
+	server.engine.GET("/room", a.room)
+	server.engine.POST("/make-room", a.makeRoom)
+	server.engine.GET("/enter-room", a.enterRoom)
+}
+
+func (a *api) roomList(context *gin.Context) {
+
+}
+
+func (a *api) makeRoom(context *gin.Context) {
+
+}
+
+func (a *api) room(context *gin.Context) {
+
+}
+
+func (a *api) enterRoom(context *gin.Context) {
+
 }
--- service/root.go
+++ service/root.go
@@ -1,6 +1,10 @@
 package service
 
-import "go_chat/repository"
+import (
+	"go_chat/repository"
+	"go_chat/types/schema"
+	"log"
+)
 
 type Service struct {
 	repository *repository.Repository
@@ -9,4 +13,40 @@ type Service struct {
 func NewService(repository *repository.Repository) *Service {
 	service := &Service{repository: repository}
 	return service
+}
+
+func (service *Service) GetChatList(roomName string) ([]*schema.Chat, error) {
+	if response, err := service.repository.GetChatList(roomName); err != nil {
+		log.Println("Failed To Get Chat List", "err", err.Error())
+		return nil, err
+	} else {
+		return response, nil
+	}
+}
+
+func (service *Service) RoomList() ([]*schema.Room, error) {
+	if res, err := service.repository.RoomList(); err != nil {
+		log.Println("Failed To Get All Room List", "err", err.Error())
+		return nil, err
+	} else {
+		return res, nil
+	}
+}
+
+func (service *Service) MakeRoom(name string) error {
+	if err := service.repository.MakeRoom(name); err != nil {
+		log.Println("Failed To Make New Room", "err", err.Error())
+		return err
+	} else {
+		return nil
+	}
+}
+
+func (service *Service) Room(name string) (*schema.Room, error) {
+	if res, err := service.repository.Room(name); err != nil {
+		log.Println("Failed To Get Room ", "err", err.Error())
+		return nil, err
+	} else {
+		return res, nil
+	}
 }
