6faff329a5049596a9b99b9538a37dda0fa1db35
HONG-JUNG-WAN <hong43ok@gmail.com>
2024-06-17 19:39:14 +0900

    feat: paging, sort 쿼리 작성

--- repository/root.go
+++ repository/root.go
@@ -32,16 +32,79 @@ func NewRepository(cfg *config.Config) (*Repository, error) {
 	return repository, nil
 }
 
+func (repository *Repository) GetChatList(roomName string) ([]*schema.Chat, error) {
+	queryString := query([]string{"SELECT * FROM", chat, "WHERE room = ? ORDER BY `when` DESC LIMIT 10"})
+
+	if cursor, err := repository.db.Query(queryString, roomName); err != nil {
+		return nil, err
+	} else {
+		defer cursor.Close()
+
+		var result []*schema.Chat
+
+		for cursor.Next() {
+			d := new(schema.Chat)
+
+			if err = cursor.Scan(&d.ID, &d.Room, &d.Name, &d.Message, &d.When); err != nil {
+				return nil, err
+			} else {
+				result = append(result, d)
+			}
+		}
+
+		if len(result) == 0 {
+			return []*schema.Chat{}, nil
+		} else {
+			return result, nil
+		}
+	}
+}
+
+func (repository *Repository) RoomList() ([]*schema.Room, error) {
+	// TODO: 페이지네이션 기능 추가 필요
+	queryString := query([]string{"SELECT * FROM", room})
+
+	if cursor, err := repository.db.Query(queryString); err != nil {
+		return nil, err
+	} else {
+		defer cursor.Close()
+
+		var result []*schema.Room
+
+		for cursor.Next() {
+			domain := new(schema.Room)
+
+			if err = cursor.Scan(&domain.ID, &domain.Name, &domain.CreateAt, &domain.UpdateAt); err != nil {
+				return nil, err
+			} else {
+				result = append(result, domain)
+			}
+		}
+
+		if len(result) == 0 {
+			return []*schema.Room{}, nil
+		} else {
+			return result, nil
+		}
+	}
+}
+
+func (repository *Repository) MakeRoom(name string) error {
+	_, err := repository.db.Exec("INSERT INTO chatting.room(name) VALUES(?)", name)
+	return err
+}
+
 func (repository *Repository) Room(name string) (*schema.Room, error) {
 	domain := new(schema.Room)
 	queryString := query([]string{})
 
-	err := repository.db.QueryRow(queryString, name).Scan(
-		&domain.ID,
-		&domain.Name,
-		&domain.CreateAt,
-		&domain.UpdateAt,
-	)
+	err := repository.db.QueryRow(queryString, name).
+		Scan(
+			&domain.ID,
+			&domain.Name,
+			&domain.CreateAt,
+			&domain.UpdateAt,
+		)
 	return domain, err
 }
 
