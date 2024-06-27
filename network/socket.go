0ca80fb1bf47c1dd01b42a054d0b45e7d9e4a6d8
jungwanhong-metanet <jungwan.hong.metanet@joyent.com>
2024-06-27 09:29:48 +0900

    feat: wss protocol test를 위한 수정 작업
    1. network.go
    2. repository/root.go
    3. service/root.go
    4. server.go
    5. socket.go

--- network/socket.go
+++ network/socket.go
@@ -5,19 +5,11 @@ package network
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/gorilla/websocket"
-	"go_chat/types"
+	"go_chat/service"
 	"log"
 	"net/http"
 	"time"
 )
-
-var Upgrader = &websocket.Upgrader{
-	ReadBufferSize:  types.SocketBufferSize,
-	WriteBufferSize: types.MessageBufferSize,
-	CheckOrigin: func(r *http.Request) bool {
-		return true
-	},
-}
 
 type Room struct {
 	Forward chan *Message // 수신되는 메시지를 보관하는 값, 들어오는 메시지를 다른 클라이언트에게 전송을 한다.
@@ -26,112 +18,123 @@ type Room struct {
 	Leave chan *Client // Socket이 끊어지는 경우에 동작
 
 	Clients map[*Client]bool // 현재 방에 있는 Client 정보를 저장
-}
 
-type Message struct {
-	Name            string
-	ChattingMessage string
-	Time            int64
+	service *service.Service
 }
 
 type Client struct {
-	Send   chan *Message
-	Room   *Room
-	Name   string
-	Socket *websocket.Conn
+	Socket *websocket.Conn // Client의 웹 소켓
+	Send   chan *Message   // 전송되는 채널
+	Room   *Room           // 클라이언트가 속해 있는 방
+	Name   string          `json:"name"`
 }
 
-func NewRoom() *Room {
+type Message struct {
+	Name    string    `json:"name"`
+	Message string    `json:"message"`
+	Room    string    `json:"room"`
+	When    time.Time `json:"when"`
+}
+
+func NewRoom(service *service.Service) *Room {
 	return &Room{
 		Forward: make(chan *Message),
-
-		Join:  make(chan *Client),
-		Leave: make(chan *Client),
-
+		Join:    make(chan *Client),
+		Leave:   make(chan *Client),
 		Clients: map[*Client]bool{},
+		service: service,
 	}
 }
 
 func (client *Client) Read() {
-	// 클라이언트가 들어오는 메시지를 읽는 함수
 	defer client.Socket.Close()
 	for {
 		var message *Message
 		err := client.Socket.ReadJSON(&message)
 		if err != nil {
-			// 클라이언트와의 WebSocket 연결이 예상치 못하게 종료되는 경우를 처리
-			if !websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway) {
-				break
-			}
-			panic(err)
-		} else {
-			log.Println("READ : ", message, "Client", client.Name)
-			log.Println()
-
-			message.Time = time.Now().Unix()
-			message.Name = client.Name
-
-			client.Room.Forward <- message
+			return
 		}
+
+		message.When = time.Now()
+		message.Name = client.Name
+		client.Room.Forward <- message
 	}
 }
 
 func (client *Client) Write() {
-	// 클라이언트가 메시지를 전송하는 함수
 	defer client.Socket.Close()
-
-	for message := range client.Send {
-		log.Println("WRITE : ", message, "Client", client.Name)
-		log.Println()
-
-		err := client.Socket.WriteJSON(message)
+	for msg := range client.Send {
+		err := client.Socket.WriteJSON(msg)
 		if err != nil {
-			panic(err)
+			return
 		}
 	}
 }
 
-func (room *Room) RunInit() {
-	// Room에 있는 모든 채널 값들을 받는 역할
+func (room *Room) Run() {
 	for {
 		select {
 		case client := <-room.Join:
-			room.Clients[client] = true
+			room.Clients[client] = true // client가 새로 들어올 떄,
 		case client := <-room.Leave:
-			room.Clients[client] = false
-			close(client.Send)
-			delete(room.Clients, client)
-		case message := <-room.Forward:
+			delete(room.Clients, client) // 나갈 때에는 map 값에서 client를 제거.
+			close(client.Send)           // 이후 client의 socker을 닫는다.
+		case msg := <-room.Forward: // 만약 특정 메시지가 방에 들어오면,
+
+			go room.service.InsertChatting(msg.Name, msg.Message, msg.Room)
+
 			for client := range room.Clients {
-				client.Send <- message
+				client.Send <- msg // 모든 client에게 전달.
 			}
 		}
 	}
 }
 
-func (room *Room) SocketServe(context *gin.Context) {
-	socket, err := Upgrader.Upgrade(context.Writer, context.Request, nil)
-	if err != nil {
-		panic(err)
+const (
+	SocketBufferSize  = 1024
+	messageBufferSize = 256
+)
+
+// 기본적으로 HTTP에 웹 소켓을 사용하려면, 이와 같이 업그레이드해 주어야 한다.
+// -> 재사용 가능하기 때문에 하나만 만들어도 된다.
+var upgrader = &websocket.Upgrader{ReadBufferSize: SocketBufferSize, WriteBufferSize: messageBufferSize}
+
+func (room *Room) ServeHTTP(c *gin.Context) {
+
+	// 이후 요청이 이렇게 들어오게 된다면 Upgrade를 통해서 소켓을 가져온다.
+	upgrader.CheckOrigin = func(r *http.Request) bool {
+		return true
 	}
 
-	userCookie, err := context.Request.Cookie("auth")
+	Socket, err := upgrader.Upgrade(c.Writer, c.Request, nil)
 	if err != nil {
-		panic(err)
+		log.Fatal("---- serveHTTP:", err)
+		return
 	}
 
+	authCookie, err := c.Request.Cookie("auth")
+	if err != nil {
+		log.Fatal("auth cookie is failed", err)
+		return
+	}
+
+	// 문제가 없다면 client를 생성하여 방에 입장했다고 채널에 전송.
 	client := &Client{
-		Send:   make(chan *Message, types.MessageBufferSize),
+		Socket: Socket,
+		Send:   make(chan *Message, messageBufferSize),
 		Room:   room,
-		Name:   userCookie.Value,
-		Socket: socket,
+		Name:   authCookie.Value,
 	}
 
 	room.Join <- client
 
+	// 또한 defer를 통해서 client가 끝날 때를 대비하여 퇴장하는 작업 연기.
 	defer func() { room.Leave <- client }()
 
+	// 이후 고루틴을 통해서 write를 실행.
 	go client.Write()
 
+	// 이후 메인 루틴에서 read를 실행함으로써 해당 요청을 닫는 것을 차단.
+	// -> 연결을 활성화. 채널을 활용
 	client.Read()
 }
